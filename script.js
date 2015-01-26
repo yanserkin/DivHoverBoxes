1d9fd8c9231efde590ebf1c75ae9d42fc8db9a69
Nick Hurst <nickthurst@gmail.com>
2015-01-26 02:34:38 -0500

    add stylesheet and script, divs created and over class added

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	for(var i = 0; i < 256; i++) {
+		if((i + 1) % 16){													// if i is not a multiple of 16
+			$("#maincontainer").append("<div class=\"squares\"></div>");	// create a div
+
+		} else {															// else
+			$("#maincontainer").append("<br>");								// add a break			
+		}
+
+		$('#maincontainer div').hover(function() {
+			$(this).toggleClass('hover_class');								// toggle hover_class css
+		})
+	}	
+})
